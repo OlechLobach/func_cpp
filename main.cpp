3f80d2b62c829e3dbb3003464fb93bbe444fbc81
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-10 17:09:31 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
-#include<iostream>
+#include <iostream>
 
-int main()
-{
+using namespace std;
 
-	return 0;
+int maxNumber(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    int result = maxNumber(num1, num2);
+
+    cout << "The maximum of " << num1 << " and " << num2 << " is: " << result << endl;
+
+    return 0;
 }
