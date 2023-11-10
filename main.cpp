642b574007f3cd08576c096d84fd9fae10f6f312
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-10 16:49:25 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
-#include<iostream>
+#include <iostream>
 
-int main()
-{
+void drawRectangle(int height, int width) {
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            if (i == 0 || i == height - 1) {
+                
+                std::cout << "- ";
+            }
+            else {
+                
+                if (j == 0 || j == width - 1) {
+                    std::cout << "| ";
+                }
+                else {
+                    std::cout << "  ";
+                }
+            }
+        }
+        std::cout << std::endl;
+    }
+}
 
-	return 0;
+int main() {
+    int height, width;
+
+    std::cout << "Enter the height of the rectangle: ";
+    std::cin >> height;
+
+    std::cout << "Enter the width of the rectangle: ";
+    std::cin >> width;
+
+    drawRectangle(height, width);
+
+    return 0;
 }
