32b188b3c8ac3a907165b218c433153d6bf197ce
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-10 17:19:50 +0200

    initila commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
-#include<iostream>
+#include <iostream>
 
-int main()
-{
+using namespace std;
 
-	return 0;
+int calculateSumInRange(int start, int end) {
+    int sum = 0;
+
+    for (int i = start; i <= end; ++i) {
+        sum += i;
+    }
+
+    return sum;
+}
+
+int main() {
+    int start, end;
+
+    cout << "Enter the starting number: ";
+    cin >> start;
+
+    cout << "Enter the ending number: ";
+    cin >> end;
+
+    int result = calculateSumInRange(start, end);
+
+    cout << "Sum of numbers in the range: " << result << endl;
+
+    return 0;
 }
