fe73c8f8b9207106739931fb5012da05cab03d25
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-10 17:05:24 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-#include<iostream>
+#include <iostream>
 
-int main()
-{
+using namespace std;
 
-	return 0;
+int cube(int n) {
+    return n * n * n;
+}
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    int result = cube(num);
+
+    cout << "Cube of " << num << ": " << result << endl;
+
+    return 0;
 }
