bc57bdd38ce9c3c206a58cea4690f28f8b949944
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-10 18:02:53 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,70 @@
-#include<iostream>
+﻿#include <iostream>
+#include <string>
 
-int main()
-{
+using namespace std;
 
-	return 0;
+bool isValidRank(const string& rank) {
+    const string validRanks[] = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+    const int numValidRanks = sizeof(validRanks) / sizeof(validRanks[0]);
+
+    for (int i = 0; i < numValidRanks; ++i) {
+        if (rank == validRanks[i]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+bool isValidSuit(char suit) {
+    const char validSuits[] = { 'C', 'D', 'H', 'S' };
+
+    for (int i = 0; i < sizeof(validSuits) / sizeof(validSuits[0]); ++i) {
+        if (suit == validSuits[i]) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void displayCard(const string& rank, char suit) {
+  
+    cout << " _______\n";
+    cout << "| " << rank << "     |\n";
+    cout << "|       |\n";
+    cout << "|   " << suit << "   |\n";
+    cout << "|       |\n";
+    cout << "|     " << rank << " |\n";
+    cout << " ‾‾‾‾‾‾‾\n";
+}
+
+int main() {
+    
+    string rank;
+    char suit;
+
+    cout << "Enter the rank of the card (e.g., 2, 10, J, Q, K, A): ";
+    cin >> rank;
+
+    
+    if (!isValidRank(rank)) {
+        cout << "Error: Invalid rank. Please enter a valid rank.\n";
+        return 1; 
+    }
+
+    cout << "Enter the suit of the card (C - clubs, D - diamonds, H - hearts, S - spades): ";
+    cin >> suit;
+
+    
+    if (!isValidSuit(suit)) {
+        cout << "Error: Invalid suit. Please enter a valid suit.\n";
+        return 1; 
+    }
+
+    
+    displayCard(rank, suit);
+
+    return 0;
 }
