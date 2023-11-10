3c736774ef7263ab099740d33c647ab2873e91fe
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-10 17:23:20 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 bool isPerfect(int number) {
-    int sum = 1; // 1 is always a divisor
+    int sum = 1; 
 
     for (int i = 2; i <= number / 2; ++i) {
         if (number % i == 0) {
