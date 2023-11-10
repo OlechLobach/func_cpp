4e7bd979d7c6582e697c584fd25388cd35d5c845
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-10 18:07:30 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
-
+using namespace :: std;
 void drawRectangle(int height, int width) {
     for (int i = 0; i < height; ++i) {
         for (int j = 0; j < width; ++j) {
             if (i == 0 || i == height - 1) {
                 
-                std::cout << "- ";
+                cout << "- ";
             }
             else {
                 
                 if (j == 0 || j == width - 1) {
-                    std::cout << "| ";
+                    cout << "| ";
                 }
                 else {
-                    std::cout << "  ";
+                    cout << "  ";
                 }
             }
         }
-        std::cout << std::endl;
+        cout << endl;
     }
 }
 
 int main() {
     int height, width;
 
-    std::cout << "Enter the height of the rectangle: ";
-    std::cin >> height;
+    cout << "Enter the height of the rectangle: ";
+    cin >> height;
 
-    std::cout << "Enter the width of the rectangle: ";
-    std::cin >> width;
+    cout << "Enter the width of the rectangle: ";
+    cin >> width;
 
     drawRectangle(height, width);
 
