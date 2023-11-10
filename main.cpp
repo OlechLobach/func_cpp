78c6b227e874a69de119bccb128a284c087525da
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-10 17:22:41 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
-#include<iostream>
+#include <iostream>
 
-int main()
-{
+using namespace std;
 
-	return 0;
+bool isPerfect(int number) {
+    int sum = 1; // 1 is always a divisor
+
+    for (int i = 2; i <= number / 2; ++i) {
+        if (number % i == 0) {
+            sum += i;
+        }
+    }
+
+    return sum == number;
+}
+
+void findPerfectNumbers(int start, int end) {
+    cout << "Perfect numbers in the interval [" << start << ", " << end << "]: ";
+
+    for (int i = start; i <= end; ++i) {
+        if (isPerfect(i)) {
+            cout << i << " ";
+        }
+    }
+
+    cout << endl;
+}
+
+int main() {
+    int start, end;
+
+    cout << "Enter the starting number: ";
+    cin >> start;
+
+    cout << "Enter the ending number: ";
+    cin >> end;
+
+    findPerfectNumbers(start, end);
+
+    return 0;
 }
