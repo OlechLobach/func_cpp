2a07d2cd6ec1624a169e8c16a09cc749ef7b8cb8
Lobach Oleg Igorovich <Lobac_va78@student.itstep.org>
2023-11-10 18:15:48 +0200

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
-#include<iostream>
+#include <iostream>
 
-int main()
-{
+using namespace :: std;
 
-	return 0;
+bool isLuckyNumber(int number) {
+    int sumFirstHalf = 0;
+    int sumSecondHalf = 0;
+
+    for (int i = 0; i < 3; ++i) {
+        sumFirstHalf += number % 10;
+        number /= 10;
+    }
+
+   
+    for (int i = 0; i < 3; ++i) {
+        sumSecondHalf += number % 10;
+        number /= 10;
+    }
+
+    return sumFirstHalf == sumSecondHalf;
+}
+
+int main() {
+    int number;
+
+    cout << "Enter a six-digit number: ";
+    cin >> number;
+
+    if (isLuckyNumber(number)) {
+        cout << number << " is a lucky number!" << endl;
+    }
+    else {
+        cout << number << " is not a lucky number." << endl;
+    }
+
+    return 0;
 }
